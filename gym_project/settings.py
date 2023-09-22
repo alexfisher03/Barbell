bf5dc83509df5189e8d087c528f7481345db583d
alexfisher03 <fisherr.alexander@gmail.com>
2023-09-21 22:00:28 -0400

    from station

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['164.90.134.67', 'localhost', '127.0.0.1', 'socialbarbell.com', 'www.socialbarbell.com']
+ALLOWED_HOSTS = ['164.90.134.67', 'socialbarbell.com', 'www.socialbarbell.com']
 
 
 # Application definition
