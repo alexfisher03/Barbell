dc7c51dfeb3678bbfc6a36f1e5e72ebc5dffb303
alexfisher03 <fisherr.alexander@gmail.com>
2023-10-03 00:55:51 -0400

    changed from decouple import config

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import Csv, config
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
