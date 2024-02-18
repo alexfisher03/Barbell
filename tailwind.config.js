0cf898de49d9ab429400c833f92ad5ea349c0caa
alexfisher03 <fisherr.alexander@gmail.com>
2024-02-18 01:27:27 -0500

    large commit, tailwind added alongside several changes

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./gym_app/templates/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
