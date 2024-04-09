3e5e953d69885f155f723c839c2e5afd2a73820e
alexfisher03 <fisherr.alexander@gmail.com>
2024-04-08 22:29:23 -0400

    Corrected LOGGING on prod_dev

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -113,14 +113,17 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/error.log',
         },
     },
-    'loggers': {                    # Change for production from dontforget.txt
-        'django': { 
-            'handlers': ['console'],
-            'level': 'INFO',
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
         },
     },
 }
