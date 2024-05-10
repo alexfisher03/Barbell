5c13e501238b6d035b7f8edc4ff4c2a2b1b93593
alexfisher03 <fisherr.alexander@gmail.com>
2024-05-10 16:49:39 -0400

    Revert prev, CSRF COOKIE SECURE = False

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -278,8 +278,8 @@ if DEBUG:
 
 
 else:
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
+    SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_HTTPONLY = False
     CSRF_COOKIE_DOMAIN = ['.socialbarbell.com']
    
