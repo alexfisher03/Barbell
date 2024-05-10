6e1fbd24f5035c2d7f60d76705ffea8811e8b08c
alexfisher03 <fisherr.alexander@gmail.com>
2024-05-10 14:51:32 -0400

    Added large patch - Webpack, Chart.js, FullCalendar

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    entry: '/gym_app/static/js/profile.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'static/dist'),
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
+    mode: 'development' // Change to production when deploying
+};
